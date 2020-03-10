2ec32290cab3046cd2038e662341200947cec3e8
C. Regis Wilson <rwilson@truecar.com>
2020-03-10 10:47:08 -0700

    Export variables for templates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-if [ -z ${BACKEND_HOST} ]; then
+if [ -z "${BACKEND_HOST}" ]; then
   echo "Error: Missing required vars (Usage: BACKEND_HOST=... $0)" >&2
   exit 1
 fi
+
+# We have to export all the variables we'll need in the templates and environment
+export BACKEND_HOST
+export SERVER_PORT
+export RESIZE_LISTEN_PORT
+export JPG_QUALITY
 
 # access logs won't print to stdout otherwise.
 # explained here: https://github.com/docker/docker/issues/19616#issuecomment-174355979
@@ -14,4 +20,5 @@ ln -sf /proc/1/fd/1 /var/log/nginx/access.log
 # https://github.com/docker/for-mac/issues/1432
 echo resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf) "ipv6=off;" > /etc/nginx/include/resolvers.conf
 
+echo Executing "$@"
 exec "$@"
