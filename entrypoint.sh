ac8d6c4209d21eaf2bceef7582e4f83065828210
C. Regis Wilson <rwilson@truecar.com>
2020-03-11 15:11:33 -0700

    Override location with spacepods vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,17 @@ if [ -z "${BACKEND_HOST}" ]; then
   exit 1
 fi
 
+if [ -z "${URL_PREFIX}" ]; then
+  echo "Error: Missing required vars (Usage: URL_PREFIX=... $0)" >&2
+  exit 1
+fi
+
 # We have to export all the variables we'll need in the templates and environment
 export BACKEND_HOST
-export SERVER_PORT
-export RESIZE_LISTEN_PORT
 export JPG_QUALITY
+export RESIZE_LISTEN_PORT
+export SERVER_PORT
+export URL_PREFIX
 
 # access logs won't print to stdout otherwise.
 # explained here: https://github.com/docker/docker/issues/19616#issuecomment-174355979
